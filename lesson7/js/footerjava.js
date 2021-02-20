2e202bace47c60daf7655c14530b687f5f698e31
jplarrea79 <71568043+jplarrea79@users.noreply.github.com>
2021-02-19 22:07:11 -0500

    Update footerjava.js

--- lesson7/js/footerjava.js
+++ lesson7/js/footerjava.js
@@ -1,2 +1,2 @@
 document.getElementById("date").innerHTML = "This was last modified on: " + document.lastModified;
-document.getElementById("year").innerHTML = "&copy" + new Date().getFullYear()+ " | Joseph Watt | Nevada | ";
+document.getElementById("year").innerHTML = "&copy" + new Date().getFullYear()+ " | Pablo Larrea | Ecuador  | ";
