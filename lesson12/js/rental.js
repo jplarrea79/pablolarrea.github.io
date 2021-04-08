b3bf435169d3f327c75003955adbcd8f7b91e31b
jplarrea79 <71568043+jplarrea79@users.noreply.github.com>
2021-04-08 00:06:55 -0500

    Update rental.js

--- lesson12/js/rental.js
+++ lesson12/js/rental.js
@@ -1,35 +1,35 @@
 
 var data = {
     "rentaltype" : [
-        { "name": "Honda Metro", "max": 1, "half": 20, "full": 30},
-        { "name": "Honda Dio", "max": 2, "half": 30, "full": 40},
-        { "name": "Honda PCX150", "max": 2, "half": 40, "full": 50},
-        { "name": "Honda Pioneer ATV", "max": 4, "half": 50, "full": 70},
+        { "name": "moto simple", "max": 1, "half": 20, "full": 30},
+        { "name": "moto doble", "max": 2, "half": 30, "full": 40},
+        { "name": "carreta", "max": 2, "half": 40, "full": 50},
+        { "name": "chiva", "max": 4, "half": 50, "full": 70},
         { "name": "Jeep Wrangler 4-Door", "max": 5, "half": 70, "full": 100},
-        { "name": "Jeep Wrangler 2-Door", "max": 4, "half": 60, "full": 85}
+        { "name": "sail", "max": 4, "half": 60, "full": 85}
 ]
 }
 
 function showData(clicked_id) {
-    if (clicked_id = "metro"){
+    if (clicked_id = "moto"){
         document.getElementById("name0").innerHTML = data.rentaltype[0].name;
         document.getElementById("max0").innerHTML = "Rider Capacity: " + data.rentaltype[0].max;
         document.getElementById("half0").innerHTML = "Half Day Rental is: $" + data.rentaltype[0].half;
         document.getElementById("full0").innerHTML = "Full Day Rental is: $" + data.rentaltype[0].full;
     }
-    if (clicked_id = "dio") {
+    if (clicked_id = "moto2") {
         document.getElementById("name1").innerHTML = data.rentaltype[1].name;
         document.getElementById("max1").innerHTML = "Rider Capacity: " + data.rentaltype[1].max;
         document.getElementById("half1").innerHTML = "Half Day Rental is: $" + data.rentaltype[1].half;
         document.getElementById("full1").innerHTML = "Full Day Rental is: $" + data.rentaltype[1].full;
     }
-    if (clicked_id = "pcx"){
+    if (clicked_id = "carreta"){
         document.getElementById("name0").innerHTML = data.rentaltype[2].name;
         document.getElementById("max0").innerHTML = "Rider Capacity: " + data.rentaltype[2].max;
         document.getElementById("half0").innerHTML = "Half Day Rental is: $" + data.rentaltype[2].half;
         document.getElementById("full0").innerHTML = "Full Day Rental is: $" + data.rentaltype[2].full;
     }
-    if (clicked_id = "h1000") {
+    if (clicked_id = "chiva") {
         document.getElementById("name1").innerHTML = data.rentaltype[3].name;
         document.getElementById("max1").innerHTML = "Rider Capacity: " + data.rentaltype[3].max;
         document.getElementById("half1").innerHTML = "Half Day Rental is: $" + data.rentaltype[3].half;
@@ -41,7 +41,7 @@ function showData(clicked_id) {
         document.getElementById("half0").innerHTML = "Half Day Rental is: $" + data.rentaltype[4].half;
         document.getElementById("full0").innerHTML = "Full Day Rental is: $" + data.rentaltype[4].full;
     }
-    if (clicked_id = "2jeep") {
+    if (clicked_id = "sail") {
         document.getElementById("name1").innerHTML = data.rentaltype[5].name;
         document.getElementById("max1").innerHTML = "Rider Capacity: " + data.rentaltype[5].max;
         document.getElementById("half1").innerHTML = "Half Day Rental is: $" + data.rentaltype[5].half;
